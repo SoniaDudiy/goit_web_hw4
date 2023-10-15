9c3e7f4b6566c0fda2e28f114009dfe026498912
Sonia <sonia09012004@gmail.com>
2023-10-15 23:20:53 +0300

    first

---
+++ main.py
@@ -0,0 +1,62 @@
+import urllib.parse
+from pathlib import Path
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+BASE_DIR = Path()
+
+
+class GoitFramework(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        route = urllib.parse.urlparse(self.path)
+        print(route.query)
+        match route.path:
+            case '/':
+                self.send_html('index.html')
+            case '/contact':
+                self.send_html('contact.html')  
+            case '/blog':
+                self.send_html('blog.html')   
+            case _:
+                file = BASE_DIR.joinpath(route.path[1:])
+                if file.exists():
+                    self.send_static(file)
+                else:
+                    self.send_html(filename:'404.html', status_code:404)    
+
+
+    def do_POST(self):
+        pass
+
+    def send_html(self, filename, status_code=200):
+        self.send_response(status_code)
+        self.send_header(keyword: 'Content: Type', value:'text/html')
+        self.end_headers()
+        with open('index.html', 'rb') as file:
+             self.wfile.write(file.read())
+
+    def send_static(self, filename, status_code=200):
+        self.send_response(status_code)
+        mine_type, *_ = minetypes.guess_type(filename)
+        if mine_type:
+            self.send_header(keyword: 'Content: Type', 'mine_type')
+        else:
+            self.send_header(keyword: 'Content: Type', value:'text/plain')
+        self.end_headers()
+        with open('index.html', 'rb') as file:
+             self.wfile.write(file.read())         
+              
+    def run_server():
+       address = ('localhost', 8080)
+       http_server = HTTPServer(address, GoitFramework)
+       try:
+           http_server.serve_forever()
+       except KeyboardInterrupt:
+           pass
+       finally: 
+           http_server.server_close()
+
+
+if __name__ == '__main__':
+    run_server()
